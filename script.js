1aee06d3d605c003579ada7b317d5c657fcc316b
Paul Cabezal <paulcabezal@gmail.com>
2022-06-01 16:11:28 -0400

    1.0 finished

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// const power = function(a, b) {
-//     return a ** b;	
-//   };
- 
-// const factorial = function(n) {
-//   if (n === 0) return 1;
-//   let product = 1;
-//   for (let i = n; i > 0; i--) {
-//     product *= i;
-//   }
-//   return product;
-// };
-
-
 // Animations
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
@@ -23,12 +9,16 @@ buttons.forEach(button => button.addEventListener('transitionend', removeTransit
 
 mainBoy = document.querySelector(`div[class=mainCalculator]`)
 
+// Display
+const updateDisplay = function(x) {
+  document.getElementById("display").innerHTML = x;
+}
+
 // Key Press Listener
 window.addEventListener('keydown', pressKey);
 
 function pressKey(e) {
   const key=document.querySelector(`div[data-key="${e.keyCode}"]`);
-  console.log(key);
   if (key) {key.classList.add('clicked');  // prevents errors on random key presses
     mathinsides(key);
   }
@@ -40,12 +30,11 @@ buttons.forEach(key => key.addEventListener('click', event => {
   mathinsides(key);
 }));
 
-
 //Calculator and Math functions
-let varA = [0];
-let varABig = 0;
-let varB = [];
-let varBBig;
+let typeNum = [];
+let typeNumConc = 0;
+let varA = 0;
+let varB;
 let operand;
 let inputStatus = 'takeA';
  
@@ -53,23 +42,17 @@ const calculate = function(a, b, operation) {
   return operation(a, b);
 }
 
-
-
-
-
-
-// come back and clean up divide by 0 sass!!
+// Math Functions
 const myFunctions = {
   add: (a,b) => a + b,
   subtract: (a, b) => a - b,
   divide: function(a,b) { 
     if (b==0) {
-      mainBoy.classList.add('hellnaw'); //testing calc flip
+      mainBoy.classList.add('hellnaw'); // calc flip on divide by 0
       setTimeout(() => {
         mainBoy.classList.remove('hellnaw');
       }, 750);            
       clearAll();
-      // alert("every time you divide by 0 an angel loses its wings");
       return 0;
     } else {
     return a / b; }
@@ -77,71 +60,83 @@ const myFunctions = {
   multiply: (a,b) => a * b,
 };
 
-const clearVars = function() {  // Sets up to perform next operation
-  varABig = calculate(varABig, varBBig, operand);
-  updateDisplay(varABig);
-  varA = [];
-  varB = [];
-  varBBig = undefined;
-}
-
-const clearAll = function(){  // Clears everything
-  varA = [];
-  varABig = undefined;
-  varB = [];
-  varBBig = undefined;
+// Clears everything
+const clearAll = function(){  
+  varA = undefined;
+  varB = undefined;
+  typeNum = [];
   operand = undefined;
   inputStatus = "takeA";
   updateDisplay('0');
 }
 
+// Sets up to perform next operation
+const clearVars = function() {  
+  varA = calculate(varA, varB, operand);
+  if (varA > 10000000000) {
+    updateDisplay('too big :(')
+  } else {
+  updateDisplay(Math.round(varA*100000)/100000);
+  }
+  typeNum = []
+  varB = undefined;
+}
+
+//Calculator inner workings
 const mathinsides = function(key) {
   if (!isNaN(Number(key.id))) {
     if (varA == []) inputStatus = "takeA"; // resets input if equal has already been pressed
     inPut(key.id);
-  } else if (key.id === "clear") {
-    clearAll(); 
-  } 
-  else if (key.id !== 'equals') {  
+
+  } else if (key.id === "clear") {  //clear all
+    clearAll();
+
+  } else if (key.id=='decimal') {  //make sure decimal can be rationally added
+    if (typeof typeNumConc == "number") { 
+      if (!typeNum.find(e => e == '.')) {
+        typeNum.push('.');
+        typeNumConc = Number(typeNum.join(''));
+        updateDisplay(typeNum.join(''));
+      }
+    }
+
+  } else if (key.id=='backspace') {   // backspace one character
+    typeNum.pop()
+    typeNumConc = Number(typeNum.join(''));
+    updateDisplay(typeNum.join(''));
+    if (inputStatus == 'takeA') {
+      varA = typeNumConc
+    } else if (inputStatus == 'takeB') {
+      varB = typeNumConc
+    }
+
+  } else if (key.id !== 'equals') {   // input operator
     inputStatus = 'takeB';
-    if (typeof varABig === "number" && typeof varBBig === "number" && operand) {
-      clearVars();
-  } else if (!varABig) {
-      varABig = 0;
-  }
+    typeNum = [];
+    if (typeof varA === "number" && typeof varB === "number" && operand) {
+      clearVars(); 
+    }
     operand = myFunctions[key.id];
-  } else if (key.id === 'equals') {
-    if (typeof varABig === "number" && typeof varBBig === "number" && operand) {
+
+  } else if (key.id === 'equals') {  // equals
+    if (typeof varA === "number" && typeof varB === "number" && operand) {
       clearVars();
       operand = undefined;
       inputStatus = "takeA";
     }
   }
+ }
+
+// translate key/mouse presses into numbers
+const inPut = function(x) {      
+  if (typeNum.length < 13) {
+      typeNum.push(Number(x));
+      typeNumConc = Number(typeNum.join(''));
+      updateDisplay(typeNumConc);
+      if (inputStatus == 'takeA') {
+        varA = typeNumConc
+      } else if (inputStatus == 'takeB') {
+        varB = typeNumConc
+      }
+    }
 }
-
-
-
-
-const inPut = function(x) {
-  if (inputStatus == "takeA") {
-    varA.push(Number(x));
-    varABig = Number(varA.join(''));
-    updateDisplay(varABig);
-  } else if (inputStatus =="takeB") {
-    varB.push(Number(x));
-    varBBig = Number(varB.join(''));
-    updateDisplay(varBBig);
-  }
-
-}
-
-
-
-  
-
-// Operations
-
-const updateDisplay = function(x) {
-  document.getElementById("display").innerHTML = x;
-}
-
